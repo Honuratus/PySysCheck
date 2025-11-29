4cbbf80ba4ae1a2fd7fb4cc00f54c74b8d840cb5
Honuratus <onurbaranyilmaz@gmail.com>
2025-11-30 01:53:18 +0300

    feat(cpu): Implement CPU probing via /proc/cpuinfo parsing

---
+++ pysyscheck.py
@@ -0,0 +1,101 @@
+import os
+import subprocess
+import json
+import datetime
+
+class PySysCheck:
+    def __init__(self):
+        # init the initial json data
+        self.system_data = {
+            "timestamp": str(datetime.datetime.now()),
+            "report_name": "PySysCheck Hardware Report",
+            "device_info": {}
+        }
+
+
+    
+    def get_cpu_info(self):
+        # add the cpu dict to json
+        self.system_data["device_info"]["cpu"] = {}
+
+        # init a cpu_data
+        cpu_data = {
+                "vendor": "Unknown",
+                "model_name": "Unknown",
+                "topology": {"physical_cores": 0, "logical_threads": 0},
+                "virtualization_support": False,
+                "cpu_family": "Unknown",
+                "cpu_model": "Unknown",
+                "cache": "Unknown"
+        }
+        try:
+            # open the cpuinfo file
+            with open("/proc/cpuinfo","r") as f:
+                # read the content
+                content = f.read()
+            
+            # parse the logical thread and check error
+            cpu_groups = content.strip().split("\n\n")
+            if not cpu_groups: return
+
+            # just take the first logical thread
+            for line in cpu_groups[0].split("\n"):
+                if ": " not in line: continue
+
+                # extract the value of each line 
+                # example line: "cpuid level     : 16" 
+                key,value = line.split(":",1)
+                value = value.strip()
+                key = key.strip()
+
+
+                # save each value in their respective position
+                if "vendor_id" in key:
+                    cpu_data['vendor'] = value
+                
+                elif "model name" in key:
+                    cpu_data['model_name'] = value
+
+                elif key=="model":
+                    if value.isdigit():
+                        cpu_data['cpu_model'] = int(value)
+
+                
+                elif "siblings" in key:
+                    if value.isdigit():
+                        cpu_data['topology']['logical_threads'] = int(value)
+
+                elif "cpu cores" in key:
+                    if value.isdigit():
+                        cpu_data['topology']['physical_cores'] = int(value)
+
+                elif "cache size" in key:
+                    cpu_data['cache'] = value
+
+                elif "cpu family" in key:            
+                    if value.isdigit():
+                        cpu_data['cpu_family'] = int(value)
+                        
+
+                elif "flags" in key:
+                    if "svm" in value or "vmx" in value:
+                        cpu_data['virtualization_support'] = True
+                
+        
+            self.system_data['device_info']['cpu'] = cpu_data
+
+        # if /proc/cpuinfo doesn't exist
+        except FileNotFoundError:
+            self.system_data['device_info']['cpu'] = {"error": "/proc/cpuinfo not found"}
+
+        # any unknown exception
+        except Exception as e:
+            self.system_data['device_info']['cpu'] = {"error": f"Parsing error: {str(e)}"}
+
+
+
+
+
+
+
+
